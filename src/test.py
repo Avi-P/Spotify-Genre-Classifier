87069688b0710b08624ff344f500cbf896619afd
Avi-P <apooruli@gmail.com>
2019-07-25 23:41:44 -0700

    Implement code to pull tracks from a Pop, Rap, and Country playlist and features of songs in it

--- src/test.py
+++ src/test.py
@@ -2,18 +2,13 @@ import spotipy
 import spotipy.oauth2 as oauth2
 import spotipy.util as util
 import os
+import csv
 
 client_id = os.environ.get('SPOTIFY_CLIENT_ID_KEY')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET_KEY')
 username = "1267918568" # My Personal Spotify User Name
 redirect = os.environ.get('SPOTIFY_REDIRECT_URL')
 scope = 'user-library-read'
-
-def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
 
 
 credentials = oauth2.SpotifyClientCredentials(client_id,
@@ -31,32 +26,164 @@ spotifyOauth = spotipy.Spotify(oauthtoken)
 spotifyPersonal = spotipy.Spotify(personalToken)
 
 if (spotifyPersonal):
-    playlists = spotifyPersonal.user_playlists(username)
+    playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:1lF6y6T2DtEsExx04J3i3z')
 
-    print(playlists)
+    with open('test.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
 
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == username:
-            print
-            print(playlist['name'])
-            print('  total tracks', playlist['tracks']['total'])
-            results = spotifyPersonal.user_playlist(username, playlist['id'],
-                                       fields="tracks,next")
-            tracks = results['tracks']
-            show_tracks(tracks)
-            while tracks['next']:
-                tracks = spotifyPersonal.next(tracks)
-                show_tracks(tracks)
-else:
-    print("Can't get token for", username)
+        spamwriter.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
+                             "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
+
+        songsURL = []
+
+        for i in range(len(playlists['items'])):
+            songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+        audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+        # print(audioFeatures)
+
+        for i in range(len(playlists['items'])):
+            song = playlists['items'][i]
+
+            spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'],
+                                 audioFeatures[i]["danceability"], audioFeatures[i]["energy"], audioFeatures[i]["key"],
+                                 audioFeatures[i]["loudness"], audioFeatures[i]["speechiness"],
+                                 audioFeatures[i]["acousticness"],
+                                 audioFeatures[i]["instrumentalness"], audioFeatures[i]["liveness"],
+                                 audioFeatures[i]["valence"],
+                                 audioFeatures[i]["tempo"], 0])
+
+# -------------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------------------------------
+
+    playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:6jMFho3Kra6mALORsAk5t0')
+
+    with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+
+        songsURL = []
+
+        for i in range(len(playlists['items'])):
+            songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+        audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+        #print(audioFeatures)
+
+
+        for i in range(len(playlists['items'])):
+            song = playlists['items'][i]
+
+            spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'], audioFeatures[i]["danceability"],audioFeatures[i]["energy"],audioFeatures[i]["key"],
+                                                 audioFeatures[i]["loudness"],audioFeatures[i]["speechiness"],audioFeatures[i]["acousticness"],
+                                                 audioFeatures[i]["instrumentalness"],audioFeatures[i]["liveness"],audioFeatures[i]["valence"],
+                                                 audioFeatures[i]["tempo"],0])
+
+
+        #-------------------
+
+        playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:6jMFho3Kra6mALORsAk5t0', offset=100)
+
+        with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',')
+
+            songsURL = []
+
+            for i in range(len(playlists['items'])):
+                songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+            audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+            # print(audioFeatures)
+
+            for i in range(len(playlists['items'])):
+                song = playlists['items'][i]
+
+                spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'],
+                                     audioFeatures[i]["danceability"], audioFeatures[i]["energy"],
+                                     audioFeatures[i]["key"],
+                                     audioFeatures[i]["loudness"], audioFeatures[i]["speechiness"],
+                                     audioFeatures[i]["acousticness"],
+                                     audioFeatures[i]["instrumentalness"], audioFeatures[i]["liveness"],
+                                     audioFeatures[i]["valence"],
+                                     audioFeatures[i]["tempo"], 0])
+
+    # -------------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------------------------------
+
+    playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:3pvmGEC1CbUF5QNfycVXjA')
+
+    with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+
+        songsURL = []
+
+        for i in range(len(playlists['items'])):
+            songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+        audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+        #print(audioFeatures)
+
+        for i in range(len(playlists['items'])):
+            song = playlists['items'][i]
+
+            spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'], audioFeatures[i]["danceability"],audioFeatures[i]["energy"],audioFeatures[i]["key"],
+                                                 audioFeatures[i]["loudness"],audioFeatures[i]["speechiness"],audioFeatures[i]["acousticness"],
+                                                 audioFeatures[i]["instrumentalness"],audioFeatures[i]["liveness"],audioFeatures[i]["valence"],
+                                                 audioFeatures[i]["tempo"],0])
+
+
+
+
+    playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:3pvmGEC1CbUF5QNfycVXjA', offset=100)
+
+    with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+
+        songsURL = []
+
+        for i in range(len(playlists['items'])):
+            songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+        audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+        # print(audioFeatures)
+
+        for i in range(len(playlists['items'])):
+            song = playlists['items'][i]
+
+            spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'],
+                                 audioFeatures[i]["danceability"], audioFeatures[i]["energy"], audioFeatures[i]["key"],
+                                 audioFeatures[i]["loudness"], audioFeatures[i]["speechiness"],
+                                 audioFeatures[i]["acousticness"],
+                                 audioFeatures[i]["instrumentalness"], audioFeatures[i]["liveness"],
+                                 audioFeatures[i]["valence"],
+                                 audioFeatures[i]["tempo"], 0])
+#-------------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------------------------------
+
+    playlists = spotifyPersonal.user_playlist_tracks(username, 'spotify:playlist:6vpLNtSBeiHzB3JODqjg79')
+
+    with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+
+        songsURL = []
+
+        for i in range(len(playlists['items'])):
+            songsURL.append(playlists['items'][i]['track']['external_urls']['spotify'])
+
+        audioFeatures = spotifyPersonal.audio_features(songsURL)
+
+        #print(audioFeatures)
+
+        for i in range(len(playlists['items'])):
+            song = playlists['items'][i]
+
+            spamwriter.writerow([(song)['track']['name'], (song)['track']['album']['artists'][0]['name'], audioFeatures[i]["danceability"],audioFeatures[i]["energy"],audioFeatures[i]["key"],
+                                                 audioFeatures[i]["loudness"],audioFeatures[i]["speechiness"],audioFeatures[i]["acousticness"],
+                                                 audioFeatures[i]["instrumentalness"],audioFeatures[i]["liveness"],audioFeatures[i]["valence"],
+                                                 audioFeatures[i]["tempo"], 0])
 
 print()
-
-if oauthtoken:
-    results = spotifyOauth.audio_analysis("spotify:track:1oJbzZINptfVVsi8SYWFqW")
-    results2 = spotifyOauth.audio_features(tracks=["spotify:track:1oJbzZINptfVVsi8SYWFqW"])
-
-    print(results)
-    print(results2)
-else:
-    print("Can't get token for", username)
