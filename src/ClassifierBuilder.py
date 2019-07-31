4152f3b43289796f64496f4c7c9f0635a6f59c92
Avi-P <apooruli@gmail.com>
2019-07-30 21:21:27 -0700

    Allow functions in ClassifierBuilder to be accessed by main and make methods to use built classifier for song/playlist analysis

--- src/ClassifierBuilder.py
+++ src/ClassifierBuilder.py
@@ -19,118 +19,120 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-class ClassifierBuilder:
+def find_best_method(log_reg_bp, svm_bp, tree_bp, forest_bp, knn_bp, nn_bp, x, y):
+    print("Performing Cross Validation")
 
-    @staticmethod
-    def build_classifier():
-        sp_data = pd.read_csv("test.csv")
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
 
-        x = sp_data[
-            ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness",
-             "Valence"]].values
-        y = sp_data['Target'].values
+    std_sc = StandardScaler()
 
-        ClassifierBuilder.find_best(x, y)
+    x_train_std = std_sc.fit_transform(x_train)
+    x_test_std = std_sc.transform(x_test)
 
-        X_std = stdsc.fit_transform(X)
+    log_reg = LogisticRegression(C=log_reg_bp["C"])
+    svm = SVC(kernel=svm_bp["kernel"], probability=True)
+    tree = DecisionTreeClassifier(max_depth=tree_bp["max_depth"])
+    forest = RandomForestClassifier(n_estimators=forest_bp["n_estimators"], random_state=0)
+    knn = KNeighborsClassifier(n_neighbors=knn_bp["n_neighbors"], weights=knn_bp["weights"])
+    nn = MLPClassifier(activation=nn_bp["activation"], hidden_layer_sizes=nn_bp["hidden_layer_sizes"])
 
-        forest.fit(X_std, y)
+    classifiers = [log_reg, knn, tree, svm, forest, nn]
+    index = ["LR", "KNN", "Tree", "SVM", "Forest", "NN"]
 
-    @staticmethod
-    def find_best(x, y):
-        classifier_list = ["Logistic Regression", "Support Vector Machine", "Decision Tree",
-                           "Forest", "K Nearest Neighbor", "Neural Network"]
+    model_scores = []
+    for clf in classifiers:
+        model_scores.append(cross_val_score(clf, x_train_std, y_train, scoring='accuracy', cv=10))
 
-        log_reg = LogisticRegression()
-        svm = SVC()
-        tree = DecisionTreeClassifier()
-        forest = RandomForestClassifier(random_state=0)
-        knn = KNeighborsClassifier()
-        nn = MLPClassifier()
+    models_df = pd.DataFrame(model_scores, columns=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                             index=index)
 
-        classifiers = [log_reg, svm, tree, forest, knn, nn]
+    models_df["Mean"] = models_df.mean(axis=1)
 
-        param_ranges = [
-            [{'C': [0.0001, 0.001, .005, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}],  # Logistic Regression
-            [{'kernel': ['rbf', 'linear']}],  # Support Vector Machine
-            [{'max_depth': [3, 5, 7]}],  # Decision Tree
-            [{'n_estimators': [10, 100, 1000]}],  # Forest
-            [{'weights': ['distance', 'uniform'], 'n_neighbors': [3, 5, 7]}],  # K Nearest Neighbors
-            [{'weights': ['distance', 'uniform'], 'n_neighbors': [3, 5, 7]}]  # Neural Network
-        ]
+    best_method = models_df["Mean"].idxmax()
+    best_index = index.index(best_method)
 
-        parameter_tuned = ClassifierBuilder.best_parameters(classifier_list, classifiers, param_ranges, x, y)
+    # for i in index:
+    #     print(models_df[i]["Mean"])
 
-        classifier = ClassifierBuilder.find_best_method(parameter_tuned[0], parameter_tuned[1], parameter_tuned[2],
-                                                        parameter_tuned[3], parameter_tuned[4], parameter_tuned[5],
-                                                        x, y)
+    print("\nBest Method Determined: " + best_method + "\n")
 
-        std_sc = StandardScaler()
-
-        x_std = std_sc.fit_transform(x)
-
-        classifier.fit(x_std, y)
-
-        # save the model to disk
-        joblib.dump(forest, 'final_forest_model.sav')
+    return classifiers[best_index]
 
 
-    @staticmethod
-    def best_parameters(classifier_list, classifiers, param_ranges, x, y):
-        x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
+def best_parameters(classifier_list, classifiers, param_ranges, x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
 
-        std_sc = StandardScaler()
+    std_sc = StandardScaler()
 
-        x_train_std = std_sc.fit_transform(x_train)
-        x_test_std = std_sc.transform(x_test)
+    x_train_std = std_sc.fit_transform(x_train)
+    x_test_std = std_sc.transform(x_test)
 
-        obj = []
+    obj = []
 
-        for i in range(0, len(classifier_list)):
-            print(classifier_list[i] + "\n")
+    for i in range(0, len(classifier_list)):
 
-            gs = GridSearchCV(estimator=classifiers[i], param_grid=param_ranges[i], scoring='accuracy', cv=10)
+        print(classifier_list[i] + "\n")
 
-            gs.fit(x_train_std, y_train)
-            train_score = gs.score(x_train_std, y_train)
-            test_score = gs.score(x_test_std, y_test)
-            print(gs.best_params_ + "\n")
+        gs = GridSearchCV(estimator=classifiers[i], param_grid=param_ranges[i], scoring='accuracy', cv=10)
 
-            print("Train score: {} \nTest score: {}\n".format(train_score, test_score))
+        gs.fit(x_train_std, y_train)
+        train_score = gs.score(x_train_std, y_train)
+        test_score = gs.score(x_test_std, y_test)
+        print(gs.best_params_)
 
-            obj.append(gs.best_params_)
+        print("Train score: {} \nTest score: {}\n".format(train_score, test_score))
 
-        return obj
+        print("----------------------------------------------------------------------")
 
-    @staticmethod
-    def find_best_method(log_reg_bp, svm_bp, tree_bp, forest_bp, knn_bp, nn_bp, x, y):
-        x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
+        obj.append(gs.best_params_)
 
-        std_sc = StandardScaler()
+    return obj
 
-        x_train_std = std_sc.fit_transform(x_train)
-        x_test_std = std_sc.transform(x_test)
 
-        log_reg = LogisticRegression(C=log_reg_bp["C"])
-        svm = SVC(kernel=svm_bp["kernel"])
-        tree = DecisionTreeClassifier(max_depth=tree_bp["max_depth"])
-        forest = RandomForestClassifier(n_estimators=forest_bp["n_estimators"], random_state=0)
-        knn = KNeighborsClassifier(n_neighbors=knn_bp["n_neighbors"], weights=knn_bp["weights"])
-        nn = MLPClassifier(activation=nn_bp["activation"], hidden_layer_sizes=nn_bp["hidden_layer_sizes"])
+def build_classifier():
+    sp_data = pd.read_csv("data.csv")
 
-        classifiers = [log_reg, knn, tree, svm, forest, nn]
-        index = ["LR", "KNN", "Tree", "SVM", "Forest", "NN"]
+    x = sp_data[
+        ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness",
+         "Valence"]].values
+    y = sp_data['Target'].values
 
-        model_scores = []
-        for clf in classifiers:
-            model_scores.append(cross_val_score(clf, x_train_std, y_train, scoring='accuracy', cv=10))
+    find_best(x, y)
 
-        models_df = pd.DataFrame(model_scores, columns=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                                 index=index)
 
-        models_df["Mean"] = models_df.mean(axis=1)
+def find_best(x, y):
+    classifier_list = ["Logistic Regression", "Support Vector Machine", "Decision Tree",
+                       "Forest", "K Nearest Neighbor", "Neural Network"]
 
-        best_method = models_df["Mean"].idxmax()
-        best_index = index.index(best_method)
+    log_reg = LogisticRegression()
+    svm = SVC()
+    tree = DecisionTreeClassifier()
+    forest = RandomForestClassifier(random_state=0)
+    knn = KNeighborsClassifier()
+    nn = MLPClassifier()
 
-        return classifiers[best_index]
+    classifiers = [log_reg, svm, tree, forest, knn, nn]
+
+    param_ranges = [
+        [{'C': [0.0001, 0.001, .005, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}],                         # Logistic Regression
+        [{'kernel': ['rbf', 'linear']}],                                                             # Support Vector Machine
+        [{'max_depth': [3, 5, 7]}],                                                                  # Decision Tree
+        [{'n_estimators': [10, 100, 1000]}],                                                         # Forest
+        [{'weights': ['distance', 'uniform'], 'n_neighbors': [3, 5, 7]}],                            # K Nearest Neighbors
+        [{'hidden_layer_sizes': [100, 250, 500, 1000], 'activation': ['relu', 'tanh', 'logistic']}]  # Neural Network
+    ]
+
+    print("----------------------------------------------------------------------")
+
+    parameter_tuned = best_parameters(classifier_list, classifiers, param_ranges, x, y)
+
+    classifier = find_best_method(parameter_tuned[0], parameter_tuned[1], parameter_tuned[2],
+                                  parameter_tuned[3], parameter_tuned[4], parameter_tuned[5],
+                                  x, y)
+
+    classifier.fit(x, y)
+
+    print("Saving model to disk\n")
+
+    # save the model to disk
+    joblib.dump(classifier, 'classifier.sav')
--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import spotipy.util as util
 import os
 import csv
 import math
-from .ClassifierBuilder import ClassifierBuilder
 
-import matplotlib as mpl
+import ClassifierBuilder
+
+import pandas as pd
+from sklearn.externals import joblib
 
 client_id = os.environ.get('SPOTIFY_CLIENT_ID_KEY')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET_KEY')
@@ -16,18 +18,23 @@ scope = 'user-library-read'
 
 def print_songs(spotify_personal, username):
 
+    print("Interacting with Spotify API\n")
+
     list_playlists = ["spotify:playlist:6aUVcyyhGJ6LZfXNYgDbC7",  # Rap
                       "spotify:playlist:4eFNbpDSEgJ7imq5IHJUou",  # Pop
                       "spotify:playlist:5ewNXA6SGPxTunHkmAVlFU",  # Country
                       "spotify:playlist:6eaaPlF4jIgG2YJt2f5IYC"]  # Metal
 
-    list_rating = [1, 0, -1, -2]
+    list_rating = [1,   # Rap
+                   0,   # Pop
+                   -1,  # Country
+                   -2]  # Metal
 
-    with open('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        spam_writer = csv.writer(csvfile, delimiter=',')
+    csv_file = open('data.csv', 'w', newline='', encoding='utf-8')
+    spam_writer = csv.writer(csv_file, delimiter=',')
 
-        spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
-                             "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
+    spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
+                         "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
 
     for i in range(0, len(list_playlists)):
         size = spotify_personal.user_playlist(username, list_playlists[i])["tracks"]["total"]
@@ -40,37 +47,142 @@ def print_songs(spotify_personal, username):
                                                               list_playlists[i],
                                                               offset=(j * 100))
 
-            with open('data.csv', 'a', newline='', encoding='utf-8') as csvfile:
-                spam_writer = csv.writer(csvfile, delimiter=',')
+            # csvfile = open('data.csv', 'a', newline='', encoding='utf-8')
+            # spam_writer = csv.writer(csvfile, delimiter=',')
 
-                songs_url = []
+            songs_url = []
 
-                for k in range(len(playlists['items'])):
-                    songs_url.append(playlists['items'][k]['track']['external_urls']['spotify'])
+            for k in range(len(playlists['items'])):
+                songs_url.append(playlists['items'][k]['track']['external_urls']['spotify'])
 
-                audio_features = spotify_personal.audio_features(songs_url)
+            audio_features = spotify_personal.audio_features(songs_url)
 
-                for k in range(len(playlists['items'])):
-                    song = playlists['items'][k]
+            for k in range(len(playlists['items'])):
+                song = playlists['items'][k]
 
-                    spam_writer.writerow([song['track']['name'],
-                                         song['track']['album']['artists'][0]['name'],
-                                         audio_features[k]["danceability"],
-                                         audio_features[k]["energy"],
-                                         audio_features[k]["key"],
-                                         audio_features[k]["loudness"],
-                                         audio_features[k]["speechiness"],
-                                         audio_features[k]["acousticness"],
-                                         audio_features[k]["instrumentalness"],
-                                         audio_features[k]["liveness"],
-                                         audio_features[k]["valence"],
-                                         audio_features[k]["tempo"],
-                                         list_rating[i]])
+                spam_writer.writerow([song['track']['name'],
+                                     song['track']['album']['artists'][0]['name'],
+                                     audio_features[k]["danceability"],
+                                     audio_features[k]["energy"],
+                                     audio_features[k]["key"],
+                                     audio_features[k]["loudness"],
+                                     audio_features[k]["speechiness"],
+                                     audio_features[k]["acousticness"],
+                                     audio_features[k]["instrumentalness"],
+                                     audio_features[k]["liveness"],
+                                     audio_features[k]["valence"],
+                                     audio_features[k]["tempo"],
+                                     list_rating[i]])
+
+    csv_file.close()
+
+    print("Done downloading data \n")
+
+
+def classify_song(spotify_personal, username, song_url):
+
+    song = [song_url]
+
+    csv_file = open('single_song.csv', 'w', newline='', encoding='utf-8')
+    spam_writer = csv.writer(csv_file, delimiter=',')
+
+    spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
+                          "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
+
+    audio_features = spotify_personal.audio_features(song)
+    track = spotify_personal.track(song[0])
+
+    spam_writer.writerow([track['name'],
+                          track['album']['artists'][0]['name'],
+                          audio_features[0]["danceability"],
+                          audio_features[0]["energy"],
+                          audio_features[0]["key"],
+                          audio_features[0]["loudness"],
+                          audio_features[0]["speechiness"],
+                          audio_features[0]["acousticness"],
+                          audio_features[0]["instrumentalness"],
+                          audio_features[0]["liveness"],
+                          audio_features[0]["valence"],
+                          audio_features[0]["tempo"]])
+
+    csv_file.close()
+
+    classifier = joblib.load('classifier.sav')
+
+    song_data = pd.read_csv("single_song.csv")
+    song_splice = song_data[["Danceability", "Energy", "Loudness",
+                             "Speechiness", "Acousticness", "Instrumentalness",
+                             "Liveness", "Valence"]].values
+
+    prediction = classifier.predict_proba(song_splice)
+
+    print(classifier.predict(song_splice))
+
+    print(prediction)
+
+
+def classify_playlist_songs(spotify_personal, username, playlist_url):
+    playlist = [playlist_url]
+
+    csv_file = open('playlist_songs.csv', 'w', newline='', encoding='utf-8')
+    spam_writer = csv.writer(csv_file, delimiter=',')
+
+    spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
+                          "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
+
+    size = spotify_personal.user_playlist(username, playlist[0])["tracks"]["total"]
+
+    num_offsets = math.ceil(size / 100)
+
+    for j in range(0, num_offsets):
+
+        playlists = spotify_personal.user_playlist_tracks(username,
+                                                          playlist[0],
+                                                          offset=(j * 100))
+
+        # csvfile = open('data.csv', 'a', newline='', encoding='utf-8')
+        # spam_writer = csv.writer(csvfile, delimiter=',')
+
+        songs_url = []
+
+        for k in range(len(playlists['items'])):
+            songs_url.append(playlists['items'][k]['track']['external_urls']['spotify'])
+
+        audio_features = spotify_personal.audio_features(songs_url)
+
+        for k in range(len(playlists['items'])):
+            song = playlists['items'][k]
+
+            spam_writer.writerow([song['track']['name'],
+                                  song['track']['album']['artists'][0]['name'],
+                                  audio_features[k]["danceability"],
+                                  audio_features[k]["energy"],
+                                  audio_features[k]["key"],
+                                  audio_features[k]["loudness"],
+                                  audio_features[k]["speechiness"],
+                                  audio_features[k]["acousticness"],
+                                  audio_features[k]["instrumentalness"],
+                                  audio_features[k]["liveness"],
+                                  audio_features[k]["valence"],
+                                  audio_features[k]["tempo"]])
+
+    csv_file.close()
+
+    classifier = joblib.load('classifier.sav')
+
+    song_data = pd.read_csv("playlist_songs.csv")
+    song_splice = song_data[["Danceability", "Energy", "Loudness",
+                             "Speechiness", "Acousticness", "Instrumentalness",
+                             "Liveness", "Valence"]].values
+
+    prediction = classifier.predict_proba(song_splice)
+
+    #print(classifier.predict(song_splice))
+
+    print(prediction)
 
 
 def main():
-
-    print(mpl.get_configdir)
 
     user = ""
 
@@ -120,14 +232,26 @@ def main():
         choice = input("Choice: ")
 
         if choice == "1":
+            playlist_url = input("Playlist URL: ")
+
+            classify_playlist_songs(spotify_personal, username, playlist_url)
+
             print()
 
         elif choice == "2":
+            song_url = input("Song URL: ")
+
+            classify_song(spotify_personal, username, song_url)
+
             print()
 
         elif choice == "3":
             print("Training (Might take a few minutes)\n")
+
+            print_songs(spotify_personal, username)
+
             ClassifierBuilder.build_classifier()
+
             print("Finished Training\n")
 
         elif choice == "4":
