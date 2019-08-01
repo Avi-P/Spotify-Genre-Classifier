d44d8603d027bf3b1c5b15fb4a6d0aa9d1effa12
Avi-P <apooruli@gmail.com>
2019-07-31 21:47:59 -0700

    Comment code and clean up

--- src/ClassifierBuilder.py
+++ src/ClassifierBuilder.py
@@ -19,16 +19,19 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+# Finds the best classifier out of all the classifiers with the best parameters
 def find_best_method(log_reg_bp, svm_bp, tree_bp, forest_bp, knn_bp, nn_bp, x, y):
     print("Performing Cross Validation")
 
+    # Splits data in training and testing dataset
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
 
     std_sc = StandardScaler()
 
+    # Finds std and mean about the data and transforms it
     x_train_std = std_sc.fit_transform(x_train)
-    x_test_std = std_sc.transform(x_test)
 
+    # Creates classifiers with the best parameters found
     log_reg = LogisticRegression(C=log_reg_bp["C"])
     svm = SVC(kernel=svm_bp["kernel"], probability=True)
     tree = DecisionTreeClassifier(max_depth=tree_bp["max_depth"])
@@ -40,70 +43,90 @@ def find_best_method(log_reg_bp, svm_bp, tree_bp, forest_bp, knn_bp, nn_bp, x, y
     index = ["LR", "KNN", "Tree", "SVM", "Forest", "NN"]
 
     model_scores = []
+
+    # Performs cross validation on all the classifiers
     for clf in classifiers:
         model_scores.append(cross_val_score(clf, x_train_std, y_train, scoring='accuracy', cv=10))
 
     models_df = pd.DataFrame(model_scores, columns=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                              index=index)
 
+    # Finds the means classification rate for all the classifier
     models_df["Mean"] = models_df.mean(axis=1)
 
+    # Finds the best classifier and best index
     best_method = models_df["Mean"].idxmax()
     best_index = index.index(best_method)
 
-    # for i in index:
-    #     print(models_df[i]["Mean"])
-
     print("\nBest Method Determined: " + best_method + "\n")
 
+    # Returns best classifier
     return classifiers[best_index]
 
 
+# Finds the best parameters for all the classifiers
 def best_parameters(classifier_list, classifiers, param_ranges, x, y):
+    # Splits the data in to training and testing
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
 
     std_sc = StandardScaler()
 
+    # fit_transform calculates standard deviation and mean and then transforms the data into z-values
     x_train_std = std_sc.fit_transform(x_train)
+    # transform only transforms the data into z-values using the std and mean from training data
     x_test_std = std_sc.transform(x_test)
 
     obj = []
 
+    # Loops through the classifiers
     for i in range(0, len(classifier_list)):
 
         print(classifier_list[i] + "\n")
 
+        # Performs grid search on all combinations of parameter ranges to find the best params
         gs = GridSearchCV(estimator=classifiers[i], param_grid=param_ranges[i], scoring='accuracy', cv=10)
 
+        # Trains the classifier on the training data
         gs.fit(x_train_std, y_train)
+
+        # Gets score for training and testing dataset
         train_score = gs.score(x_train_std, y_train)
         test_score = gs.score(x_test_std, y_test)
+
         print(gs.best_params_)
 
         print("Train score: {} \nTest score: {}\n".format(train_score, test_score))
 
         print("----------------------------------------------------------------------")
 
+        # Adds it to list of classifiers
         obj.append(gs.best_params_)
 
     return obj
 
 
+# Entry method
 def build_classifier():
+    # Reads the data from the CSV
     sp_data = pd.read_csv("data.csv")
 
+    # Gets the values of the features we want to classify
     x = sp_data[
         ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness",
          "Valence"]].values
     y = sp_data['Target'].values
 
+    # Calls wrapper method
     find_best(x, y)
 
 
+# Wrapper method to other methods
 def find_best(x, y):
+    # List of classifiers we will be testing
     classifier_list = ["Logistic Regression", "Support Vector Machine", "Decision Tree",
                        "Forest", "K Nearest Neighbor", "Neural Network"]
 
+    # Creates the classifiers
     log_reg = LogisticRegression()
     svm = SVC()
     tree = DecisionTreeClassifier()
@@ -111,8 +134,10 @@ def find_best(x, y):
     knn = KNeighborsClassifier()
     nn = MLPClassifier()
 
+    # Stores them into an list
     classifiers = [log_reg, svm, tree, forest, knn, nn]
 
+    # List of list with all the parameters we want to test for the classifiers
     param_ranges = [
         [{'C': [0.0001, 0.001, .005, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]}],                         # Logistic Regression
         [{'kernel': ['rbf', 'linear']}],                                                             # Support Vector Machine
@@ -124,15 +149,18 @@ def find_best(x, y):
 
     print("----------------------------------------------------------------------")
 
+    # Returns an list with all the classifiers instantiated with the best parameters found
     parameter_tuned = best_parameters(classifier_list, classifiers, param_ranges, x, y)
 
+    # Finds the best classifier out of all the classifiers
     classifier = find_best_method(parameter_tuned[0], parameter_tuned[1], parameter_tuned[2],
                                   parameter_tuned[3], parameter_tuned[4], parameter_tuned[5],
                                   x, y)
 
+    # Trains the best classifier on all the data
     classifier.fit(x, y)
 
     print("Saving model to disk\n")
 
-    # save the model to disk
+    # Save the model to disk
     joblib.dump(classifier, 'classifier.sav')
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import spotipy
-import spotipy.oauth2 as oauth2
 import spotipy.util as util
+
 import os
 import csv
 import math
@@ -10,16 +10,23 @@ import ClassifierBuilder
 import pandas as pd
 from sklearn.externals import joblib
 
+# Keys to get from System Variables that allow access to Spotify API
 client_id = os.environ.get('SPOTIFY_CLIENT_ID_KEY')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET_KEY')
 redirect = os.environ.get('SPOTIFY_REDIRECT_URL')
+
+# Scope for personal token usage
 scope = 'user-library-read'
 
 
+# Interacts with Spotify API and gets data for all the songs in 4
+# playlists where each playlist represents Rap, Pop, Rock, and Country
+# Writes all the data to a CSV
 def print_songs(spotify_personal, username):
 
     print("Interacting with Spotify API\n")
 
+    # Spotify Playlist URLs
     list_playlists = ["spotify:playlist:6aUVcyyhGJ6LZfXNYgDbC7",  # Rap
                       "spotify:playlist:4eFNbpDSEgJ7imq5IHJUou",  # Pop
                       "spotify:playlist:5ewNXA6SGPxTunHkmAVlFU",  # Country
@@ -30,33 +37,40 @@ def print_songs(spotify_personal, username):
                    -1,  # Country
                    -2]  # Metal
 
+    # CSV File writer
     csv_file = open('data.csv', 'w', newline='', encoding='utf-8')
     spam_writer = csv.writer(csv_file, delimiter=',')
 
+    # Columns in CSV File
     spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
                          "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
 
+    # Cycles through the list of playlists and prints the songs.
     for i in range(0, len(list_playlists)):
+        # Gets number of songs in playlist
         size = spotify_personal.user_playlist(username, list_playlists[i])["tracks"]["total"]
 
+        # Need to calculate how many times to do offset since only 100 songs' data can be gotten at a time
         num_offsets = math.ceil(size / 100)
 
+        # Loops through playlist with offsets
         for j in range(0, num_offsets):
 
+            # Gets JSON response
             playlists = spotify_personal.user_playlist_tracks(username,
                                                               list_playlists[i],
                                                               offset=(j * 100))
 
-            # csvfile = open('data.csv', 'a', newline='', encoding='utf-8')
-            # spam_writer = csv.writer(csvfile, delimiter=',')
-
             songs_url = []
 
+            # Extracts all the song URLs from the response to run through another API endpoint
             for k in range(len(playlists['items'])):
                 songs_url.append(playlists['items'][k]['track']['external_urls']['spotify'])
 
+            # Gets audio features for all the songs
             audio_features = spotify_personal.audio_features(songs_url)
 
+            # Loops through the audio features for all the songs and writes them to CSV File
             for k in range(len(playlists['items'])):
                 song = playlists['items'][k]
 
@@ -74,24 +88,30 @@ def print_songs(spotify_personal, username):
                                      audio_features[k]["tempo"],
                                      list_rating[i]])
 
+    # Closes CSV File so it can be used immediately
     csv_file.close()
 
     print("Done downloading data \n")
 
 
-def classify_song(spotify_personal, username, song_url):
+# Gets audio features for the song inputted and classifies the genre
+def classify_song(spotify_personal, song_url):
 
     song = [song_url]
 
+    # CSV File writer
     csv_file = open('single_song.csv', 'w', newline='', encoding='utf-8')
     spam_writer = csv.writer(csv_file, delimiter=',')
 
+    # Columns in CSV File
     spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
                           "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
 
+    # Gets audio features for the song and the name of the song
     audio_features = spotify_personal.audio_features(song)
     track = spotify_personal.track(song[0])
 
+    # Writes the audio characteristics of the songs to CSV
     spam_writer.writerow([track['name'],
                           track['album']['artists'][0]['name'],
                           audio_features[0]["danceability"],
@@ -105,51 +125,64 @@ def classify_song(spotify_personal, username, song_url):
                           audio_features[0]["valence"],
                           audio_features[0]["tempo"]])
 
+    # Closes CSV File so it can be used immediately
     csv_file.close()
 
+    # Gets machine learning model from file system
     classifier = joblib.load('classifier.sav')
 
+    # Reads the data of the song
     song_data = pd.read_csv("single_song.csv")
     song_splice = song_data[["Danceability", "Energy", "Loudness",
                              "Speechiness", "Acousticness", "Instrumentalness",
                              "Liveness", "Valence"]].values
 
+    # Using the model classifies the genre of the song
     prediction = classifier.predict_proba(song_splice)
 
+    # Prints prediction
     print(classifier.predict(song_splice))
 
+    # Prints the probabilities of all the prediction
     print(prediction)
 
 
+# Classifies all the songs in a playlist
 def classify_playlist_songs(spotify_personal, username, playlist_url):
     playlist = [playlist_url]
 
+    # CSV File writer
     csv_file = open('playlist_songs.csv', 'w', newline='', encoding='utf-8')
     spam_writer = csv.writer(csv_file, delimiter=',')
 
+    # Columns in CSV File
     spam_writer.writerow(["Song_Name", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness",
                           "Acousticness", "Instrumentalness", "Liveness", "Valence", "Temp", "Target"])
 
+    # Gets number of songs in playlist
     size = spotify_personal.user_playlist(username, playlist[0])["tracks"]["total"]
 
+    # Need to calculate how many times to do offset since only 100 songs' data can be gotten at a time
     num_offsets = math.ceil(size / 100)
 
+    # Loops through playlist with offsets
     for j in range(0, num_offsets):
 
+        # Gets JSON response
         playlists = spotify_personal.user_playlist_tracks(username,
                                                           playlist[0],
                                                           offset=(j * 100))
 
-        # csvfile = open('data.csv', 'a', newline='', encoding='utf-8')
-        # spam_writer = csv.writer(csvfile, delimiter=',')
-
         songs_url = []
 
+        # Extracts all the song URLs from the response to run through another API endpoint
         for k in range(len(playlists['items'])):
             songs_url.append(playlists['items'][k]['track']['external_urls']['spotify'])
 
+        # Gets audio features for all the songs
         audio_features = spotify_personal.audio_features(songs_url)
 
+        # Loops through the audio features for all the songs and writes them to CSV File
         for k in range(len(playlists['items'])):
             song = playlists['items'][k]
 
@@ -166,19 +199,24 @@ def classify_playlist_songs(spotify_personal, username, playlist_url):
                                   audio_features[k]["valence"],
                                   audio_features[k]["tempo"]])
 
+    # Closes CSV File so it can be used immediately
     csv_file.close()
 
+    # Gets machine learning model from file system
     classifier = joblib.load('classifier.sav')
 
+    # Reads the data of the songs
     song_data = pd.read_csv("playlist_songs.csv")
     song_splice = song_data[["Danceability", "Energy", "Loudness",
                              "Speechiness", "Acousticness", "Instrumentalness",
                              "Liveness", "Valence"]].values
 
+    # Using the model classifies the genre of the songs
     prediction = classifier.predict_proba(song_splice)
 
-    #print(classifier.predict(song_splice))
+    # print(classifier.predict(song_splice))
 
+    # Prints prediction
     print(prediction)
 
 
@@ -186,6 +224,7 @@ def main():
 
     user = ""
 
+    # Used to store a username in a text file and access it
     try:
         file = open('user.txt')
 
@@ -193,6 +232,8 @@ def main():
             user = file.read()
 
         file.close()
+
+    # Handles error where there is no username saved
     except FileNotFoundError:
         print("We need your username to proceed: ")
         user = input()
@@ -204,25 +245,23 @@ def main():
 
         file.close()
 
-    credentials = oauth2.SpotifyClientCredentials(client_id,
-                                                  client_secret)
-
+    # Used to access user data
     personal_token = util.prompt_for_user_token(user,
                                                 scope,
                                                 client_id,
                                                 client_secret,
                                                 redirect)
 
-    oauth_token = credentials.get_access_token()
-
-    spotify_oauth = spotipy.Spotify(oauth_token)
+    # API Wrapper objects to use functions to interact with Spotify API
     spotify_personal = spotipy.Spotify(personal_token)
 
+    # Username of person logged in
     username = spotify_personal.current_user()["id"]
 
-    exit = False
+    exit_var = False
 
-    while exit == False:
+    # Loop for interface
+    while not exit_var:
         print("Choose an option")
         print("1 - Analyze a playlist")
         print("2 - Analyze a song")
@@ -231,6 +270,7 @@ def main():
 
         choice = input("Choice: ")
 
+        # Allows user to input a playlist and get back predictions
         if choice == "1":
             playlist_url = input("Playlist URL: ")
 
@@ -238,13 +278,15 @@ def main():
 
             print()
 
+        # Allows user to input a song and get back a prediction
         elif choice == "2":
             song_url = input("Song URL: ")
 
-            classify_song(spotify_personal, username, song_url)
+            classify_song(spotify_personal, song_url)
 
             print()
 
+        # Retrains model
         elif choice == "3":
             print("Training (Might take a few minutes)\n")
 
@@ -254,12 +296,15 @@ def main():
 
             print("Finished Training\n")
 
+        # Exit
         elif choice == "4":
-            exit = True
+            exit_var = True
 
+        # Handles any invalid input
         else:
             print("Invalid Input.\n")
 
 
+# Program entrance
 if __name__ == "__main__":
     main()
